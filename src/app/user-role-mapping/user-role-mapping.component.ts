40d597bdae197c927090663dc0332a3e42b6a7e9
marishvinoj <43641320+marishvinoj@users.noreply.github.com>
2023-12-31 19:24:53 +0530

    UserRoleMapping

--- src/app/user-role-mapping/user-role-mapping.component.ts
+++ src/app/user-role-mapping/user-role-mapping.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserRoleMapping } from '../models/userrolemapping';
+import { UserRoleMappingService } from '../services/userrolemapping.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-user-role-mapping',
@@ -21,18 +24,32 @@ export class UserRoleMappingComponent implements OnInit {
     { id: 3, name: 'Role 3' }
   ];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, 
+    private userRoleMappingService: UserRoleMappingService,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.userRoleForm = this.formBuilder.group({
-      UserId: ['', Validators.required],
-      roles: [[], Validators.required]
+      UserID: ['', Validators.required],
+      Roles: [[], Validators.required]
     });
   }
 
   onSubmit(): void {
+    // if (this.userRoleForm.valid) {
+    //   console.log(this.userRoleForm.value);
+    // }
     if (this.userRoleForm.valid) {
-      console.log(this.userRoleForm.value);
+      const userRoleMapping: UserRoleMapping = this.userRoleForm.value;
+      this.userRoleMappingService.addUserRoleMapping(userRoleMapping).subscribe(
+        () => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User added successfully' });
+          this.userRoleForm.reset();
+        },
+        (error) => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to add user' });
+        }
+      );
     }
   }
 }
